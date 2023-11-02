7d5e9d24a936ed1a0ff10eb1d0f375acbb52ebf5
victor247k <93835023+victor247k@users.noreply.github.com>
2023-11-02 19:16:47 +0200

    hopefully it's fixed

--- app.js
+++ app.js
@@ -164,10 +164,10 @@ const clouds = [];
 
 // Images
 const grassImage = new Image();
-grassImage.src = '/images/grass.jpg';
+grassImage.src = 'images/grass.jpg';
 
 const balloonImage = new Image();
-balloonImage.src = '/images/balloon.png'
+balloonImage.src = 'images/balloon.png'
 
 // Animation 
 function animate(timestamp) {
